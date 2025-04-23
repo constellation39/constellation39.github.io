792f8ba3351457ab4561868c741886242354014d
constellation39 <constellation39@outlook.com>
2025-04-23 23:58:05 +0800

    fix: Corrected subtitle and bio in site and profile config

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
 	title: "Constellation39",
-	subtitle: "⭐star was here❤",
+	subtitle: "⭐stars was here❤",
 	lang: "zh_CN", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
@@ -54,7 +54,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Constellation39",
-	bio: "⭐star was here❤",
+	bio: "⭐stars was here❤",
 	links: [
 		{
 			name: "GitHub",
